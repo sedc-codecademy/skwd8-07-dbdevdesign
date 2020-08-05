140250b99a66ba2864168f76670a322536b6bd9b
Tosho Todorovski <tosho.todorovski@navetti.com>
2020-08-05 22:21:23 +0200

    Added class 01

---
+++ g6/Day 1/Scripts/Session 1 - 02 DDL and DML.sql
@@ -0,0 +1,23 @@
+--------------
+-- DDL ACTIONS
+--------------
+
+--1. Open Database Diagrams section
+
+--2. New diagram
+
+--3. create new table with name - EmployeeFromDiagram
+
+--4. Add Columns Id, Name, Description, DateOfBirth
+
+--5. Save the diagram
+
+--------------
+-- DML ACTIONS
+--------------
+
+--1. Find the new table in Tables section
+
+--2. Right click - Edit top 200 Rows
+
+--3. Insert some data
