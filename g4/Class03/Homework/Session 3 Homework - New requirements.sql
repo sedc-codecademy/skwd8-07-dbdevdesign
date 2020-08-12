fc1e895ac247bd5961b68d03f0d6a3bac3a76477
Kristina.Spasevska <kristina.spasevska@sourcico.com>
2020-08-12 09:54:52 +0200

    new homework added

---
+++ g4/Class03/Homework/Session 3 Homework - New requirements.sql
@@ -0,0 +1,17 @@
+/*
+--  List all student First Name that are duplicated more than 3 time
+
+--  List the First Name for all the student's that their Last Name is duplicated and they are born in 1985  (Hint use View)
+
+--  Find how many courses each student (using Name and Last Name) has attended
+
+--  Find the students (using Name and Last Name) that have attended less than 20 courses and get their Min, Max and Average grade ordered by the Average Grade ascending
+
+--  For all the students (using First Name and Last Name) get their Min, Max and Average grade (Create view vv_StudentSuccess)
+
+--  Find all the students that have achieved less than 25% from the AchievementMaxPoints for the AchievementType  'Domasni'
+
+--  For the Students from the above list check what is their Success (Min, Max and Average grade) and order them by the AVG grade
+ */
+
+
