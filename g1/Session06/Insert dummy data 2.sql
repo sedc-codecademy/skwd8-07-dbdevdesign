c27371b18531d2ce51556e60654a7ca7859ed674
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-08-18 17:22:52 +0200

    altered script

--- g1/Session06/Insert dummy data 2.sql
+++ g1/Session06/Insert dummy data 2.sql
@@ -1,27 +1,27 @@
 --3-- insert values
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(1,'200500222333','Partizanski Odredi 15','070555666','aaa@vitalia.com','Nema zabeleshka')
+values(1,'200500222332','XXXX 16','070555666','aaa@vitalia.com','Nema zabeleshka')
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(2,'200500222334','XXXX 15','070779523','bbb@vitalia.com',NULL)
+values(2,'200500222303','XXXX 15','070779523','bbb@vitalia.com',NULL)
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(3,'300500222335','XXXX 16','070779523','ccc@vitalia.com','TEST 1')
+values(3,'300500222311','XXXX 16','070779523','ccc@vitalia.com','TEST 1')
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(4,'210500222335','XXXX 17','070779523','ddd@vitalia.com',NULL)
+values(4,'210500222374','XXXX 17','070779523','ddd@vitalia.com',NULL)
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(5,'300500222336','XXXX 18','070779544','vvv@vitalia.com','Nema plateno faktura')
+values(5,'30050022222','XXXX 18','070779544','vvv@vitalia.com','Nema plateno faktura')
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(6,'200500222337','XXXX 19','070779555','ddd@vitalia.com',NULL)
+values(6,'200500222331','XXXX 19','070779555','ddd@vitalia.com',NULL)
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(7,'300500222338','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
+values(7,'300500222325','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(1,'200500222338','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
+values(1,'200500222363','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
 values(2,'500500222338','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
@@ -30,5 +30,5 @@ insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address]
 values(3,'200500222338','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
 go
 insert into dbo.businessentity_details (BusinessentityID,AccountNumber,[Address],Contact,email,comment)
-values(4,'200500222338','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
+values(4,'200500222375','XXXX 20','070779566','aa@vitalia.com','Nevaliden kontakt')
 go
