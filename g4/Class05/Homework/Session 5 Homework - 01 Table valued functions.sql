e256358a25019315526570809cfb7dfc4ed2783b
Kristina Spasevska <spasevska.kristina87@gmail.com>
2020-08-21 20:47:50 +0200

    scripts added class06 g4

--- g4/Class05/Homework/Session 5 Homework - 01 Table valued functions.sql
+++ g4/Class05/Homework/Session 5 Homework - 01 Table valued functions.sql
@@ -1,4 +1,28 @@
-/*
-Create multi-statement table value function that for specific Teacher and Course will return list of students (FirstName, LastName) who passed the exam, together with Grade and CreatedDate
+DROP FUNCTION IF EXISTS dbo.fn_StudentGradesPerTeacherCourse;
+GO
 
-*/
+CREATE FUNCTION dbo.fn_StudentGradesPerTeacherCourse (@TeacherID smallint, @CourseId smallint)
+RETURNS @output TABLE (StudentFirstName nvarchar(50), StudentLastName nvarchar(50), Grade tinyint, CreatedDate datetime)
+AS
+BEGIN
+
+INSERT INTO @output
+SELECT
+	s.FirstName as StudentFirstName, s.LastName as StudentLastName, g.Grade, g.CreatedDate
+FROM
+	Grade as g
+	INNER JOIN Student as s ON g.StudentID = s.ID
+WHERE
+	TeacherID = @TeacherID and CourseID = @CourseId
+
+RETURN 
+END
+
+GO
+
+-- Execution
+declare @TeacherId int = 1
+declare @CourseId int = 1
+
+select * from dbo.fn_StudentGradesPerTeacherCourse (@TeacherId, @CourseId)
+order by CreatedDate, StudentFirstName, StudentLastName
